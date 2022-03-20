1b00c180649fbb7e9160e95ea44cb5a6ffeb1c39
Gabriel <92824009+GabrielViola95@users.noreply.github.com>
2022-03-19 21:48:55 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,7 @@ import  { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVujiuW1JiU7OQA8tkn4pLTjrw6dNLszE",
-  authDomain: "fb-crud-react-2ecf4.firebaseapp.com",
-  projectId: "fb-crud-react-2ecf4",
-  storageBucket: "fb-crud-react-2ecf4.appspot.com",
-  messagingSenderId: "145343448877",
-  appId: "1:145343448877:web:76593d23577536a86575a4"
+  //NO INFORMATION - INFO HIDDEN
 };
 
 // Initialize Firebase
