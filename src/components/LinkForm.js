7d1e6435ff6125e9d54f34688a73dcf2c2743668
GabrielViola95 <92824009+GabrielViola95@users.noreply.github.com>
2022-03-19 21:57:15 -0300

    fb config

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import add_link from '../assets/add_link.svg';
 import edit_black from '../assets/edit_black.svg';
 
-const LinkForm = () => {
+    const initialStateValues = {
+        url: "",
+        name: "",
+        description: "",
+    }
+
+const LinkForm = ({addTask}) => {
+
+    const [values, setValues] = useState(initialStateValues);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(values);
+        addTask(values);
+    };
+
+    const handleInputChange = (e)=> {
+        const {name, value} = e.target;
+        setValues({...values, [name]:value})
+    }
+
   return (
-    <form className='card card-body'>
+    <form className='card card-body' onSubmit={handleSubmit}>
         <div className="m-1 form-group input-group">
             <div className="input-group-text bg-light">
-                <img src={add_link} alt="" />
+                <img className='material-icons' src={add_link} alt="add" />
             </div>
-            <input type="text" className='form-control' placeholder='https://ejemplo.com' name='url' />
+            <input type="text" className='form-control' placeholder='https://ejemplo.com' name='url' onChange={handleInputChange} />
         </div>
         <div className="m-1 form-group input-group">
             <div className="input-group-text bg-light">
-                <img src={edit_black} alt="" />
+                <img className='material-icons' src={edit_black} alt="edit" />
             </div>
-            <input type="text" className='form-control' placeholder='Nombre del sitio' name='name' />
+            <input type="text" className='form-control' placeholder='Nombre del sitio' name='name' onChange={handleInputChange} />
         </div>
         <div className="m-1 form-group">
-            <textarea name="description" rows="3" className='form-control'></textarea>
+            <textarea name="description" rows="3" className='form-control' placeholder='Escribe una descripción' onChange={handleInputChange}></textarea>
         </div>
+        <button className='btn btn-primary btn-block'>
+            Guardar
+        </button>
     </form>
   )
 }
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import LinkForm from './LinkForm';
+import { db } from '../firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
 
 const Links = () => {
+
+  const usersCollectionRef = collection(db, "Links");
+
+    const addTask = async () => {
+        const data = await getDocs(usersCollectionRef)
+        console.log(data)
+    }
+
   return (
-    <div>Links</div>
+    <div className='container p-4'>
+        <LinkForm addTask={addTask} />
+    </div>
   )
 }
 
